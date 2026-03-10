9d5708d979ed19fc04d89761202f9714402a6a9a
RajeshNagar701 <rajeshnagar.tops@gmail.com>
2026-03-10 16:07:36 +0530

    commit

---
+++ Next_js/my-app/app/layout.tsx
@@ -0,0 +1,18 @@
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+      </head>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ Next_js/my-app/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="bg-danger">
+      <h1>Hi i am next.js page</h1>
+    </div>
+	);
+}
