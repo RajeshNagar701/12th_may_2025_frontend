82c1b56bfc30b99f017a8df399419b30b9e0e27e
RajeshNagar701 <rajeshnagar.tops@gmail.com>
2026-05-21 10:00:56 +0530

    change

--- Node/10_Mongoose/Mongoose_crud/index.js
+++ Node/10_Mongoose/Mongoose_crud/index.js
@@ -2,16 +2,49 @@
 var mongoose = require('mongoose'); // import mongoose module
 mongoose.connect('mongodb://localhost:27017/12th_may_mern'); // conect mongodb
 
-var express=require('express');
-const app=express();
+var express = require('express');
+const app = express();
 app.use(express.json());
 
 // validation schema
-var user_schema=mongoose.Schema({	
-	userId: Number,
-	id:Number,
-	title:String,
-	body:String
+/*
+var emp_schema=mongoose.Schema({	
+	name:{
+		type: String,
+		required: true},
+	email:{
+		type: String,
+		unique: true,
+		required: true},
+	password:{
+		type: String,
+		required: true},
+	mobile:{ 
+		type: String,
+		required: [true, "Mobile number is required"],
+		match: [/^[0-9]{10}$/, "Enter valid 10 digit mobile number"]},
+	dob:{ 
+		type: Date,default: Date.now}
+});
+*/
+
+var user_schema =new  mongoose.Schema({
+	userId: {
+		type: Number,
+		required: [true, "UserId number is required"]
+	},
+	id: {
+		type: Number,
+		required: [true, "UserId number is required"]
+	},
+	title: {
+		type: String,
+		required: [true, "Title is required"]
+	},
+	body: {
+		type: String,
+		required: [true, "Body is required"]
+	}
 });
 
 // create model & apply schema on model
@@ -20,32 +53,49 @@ var user_model = mongoose.model('users', user_schema);
 // get all data
 app.get("/get_users", async (req, resp) => {
 	let result = await user_model.find()
-    resp.send(result);
+	resp.send(result);
 });
 
 // get data by column
 app.get("/get_colum", async (req, resp) => {
-    let data = await user_model.find({userId:1});
-    resp.send(data);
+	let data = await user_model.find({ userId: 1 });
+	resp.send(data);
 })
 
 // get data by id  http://localhost:5000/get_params/69f1826acefb9d39e7cc6a89
 app.get("/get_params/:_id", async (req, resp) => {
 	console.log(req.params)
-    let data = await user_model.find({_id:req.params});
-    resp.send(data);
+	let data = await user_model.find({ _id: req.params });
+	resp.send(data);
 })
 
 // search data by patern column  http://localhost:5000/get_search/raj
-app.get("/get_search/:pattern", async (req, resp) => {	
+app.get("/get_search/:pattern", async (req, resp) => {
 	//console.log(req.params.key);
 	let data = await user_model.find({
-		"$or":[
-                {title:{$regex:req.params.pattern,$options: "i" }},  //$option  for case incensitive
-				{body:{$regex:req.params.pattern,$options: "i" }}
-              ]
+		"$or": [
+			{ title: { $regex: req.params.pattern, $options: "i" } },  //$option  for case incensitive
+			{ body: { $regex: req.params.pattern, $options: "i" } }
+		]
 	});
-    resp.send(data);
+	resp.send(data);
 });
+
+// get data by id  http://localhost:5000/post_data
+app.post("/post_data", async (req, resp) => {
+    try {
+        let data = new user_model(req.body);
+        const result = await data.save();
+        console.log(result);
+        resp.status(201).send(result);
+
+    } catch (error) {
+        console.log(error);
+        resp.status(500).send({
+            message: error.message
+        });
+    }
+});
+
 
 app.listen(5000);
